d93e72f07d8511fe5c1cc33f8ee1ab011976647b
Sylvia Mutoni <slyviamutoni67@gmail.com>
2026-07-17 05:25:51 +0200

    calculateage

---
+++ grade.js
@@ -0,0 +1,14 @@
+function calculategrade(score){
+    if (score>=90){
+    return "A"}
+    else if(score>=80){
+        return "B"}
+        else if (score >= 70){
+            return "C"}
+            else if (score>=60){
+                return "D"}
+                else (score >60)
+                return "E"    
+                }
+        
+console.log(calculategrade(56));
---
+++ string.js
@@ -0,0 +1,20 @@
+// Problem
+
+// Write a function named checkNumber that takes a number and returns:
+
+// "Positive" if the number is greater than zero.
+
+// "Negative" if the number is less than zero.
+// "Zero" if the number is equal to zero.
+
+
+function checkNumber(num){
+  if (num==0){
+  return "Zero";}
+  else if(num>0) 
+  return "positive";
+  else  (num<0)
+  return "negative";
+}
+console .log(checkNumber(345))
+
